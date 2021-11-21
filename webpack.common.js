cc5561e650ae8f4837008675aef4f1529e4a82d3
Marlon Ruttmann <m.ruttmann@hotmail.com>
2021-11-21 20:44:41 -0300

    Minify JS, CSS and HTML in production
    - Extracted CSS into own file in production
    
    - Minified CSS in production
    
    - Add TerserJS back in to minify JS in production
    
    - Minified HTML in production as well

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: {
     main: "./src/index.js",
-    vendor: "./src/vendor.js"
+    vendor: "./src/vendor.js",
   },
-  plugins: [new HtmlWebpackPlugin({template: './src/template.html'})],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // 3. Inject styles into DOM
-          'css-loader', // 2. Turns css into commonjs
-          'sass-loader' // 1. Turns sass into css
-        ]
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,23 @@
-const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "development", // Do not minify the bundle
   devServer: { // No need for output configs since there's no file system bundling for dev server
     contentBase: './dist'
+  },
+  plugins: [new HtmlWebpackPlugin({template: './src/template.html'})],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // 3. Inject styles into DOM
+          'css-loader', // 2. Turns css into commonjs
+          'sass-loader' // 1. Turns sass into css
+        ]
+      }
+    ]
   }
 })
